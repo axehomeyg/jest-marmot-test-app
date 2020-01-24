137008f5266ee001c880688721f19cf7c1731d76
Michael Cerna <cerna5150@yahoo.com>
2020-01-24 16:30:24 -0600

    Updated test with working jest-marmot

--- __tests__/session.test.js
+++ __tests__/session.test.js
@@ -1,44 +1,42 @@
 import React from 'react'
-import Marmot, {scenario} from 'jest-marmot'
-// import App from '../src/components/app'
+import Marmot, {scenario, stubMessageChannel} from 'jest-marmot'
+import App from '../src/components/app'
 
 // Redux/Router Mocks
 import { applyMiddleware, createStore }  from 'redux'
 import { Provider } from 'react-redux' // eslint-disable-line
 import thunk from 'redux-thunk'
-// import reducer from '../src/reducers'
+import reducer from '../src/reducers'
 import { MemoryRouter as Router } from 'react-router-dom' // eslint-disable-line
-// import history from '../src/history'
+import history from '../src/history'
 
-window.MessageChannel = require('worker_threads').MessageChannel;
-
+stubMessageChannel()
 /*
  * Mock Redux
  */
-// const createMockStore = (initialState = {}) => createStore(reducer, initialState, applyMiddleware(thunk))
-//
-// const withProvider = store => child => {
-//   return (<Provider store={store}>{child}</Provider>)
-// }
+const createMockStore = (initialState = {}) => createStore(reducer, initialState, applyMiddleware(thunk))
+
+const withProvider = store => child => {
+  return (<Provider store={store}>{child}</Provider>)
+}
 
 /*
  * Mock Router
  */
-// const withRouter = (child, initialRoute) => {
-//   history.replace(initialRoute)
-//   return <Router initialEntries={[initialRoute || "/"]}>{child}</Router>
-// }
+const withRouter = (child, initialRoute) => {
+  history.replace(initialRoute)
+  return <Router initialEntries={[initialRoute || "/"]}>{child}</Router>
+}
 
 /*
  * Common Steps
  */
-
-// const common = ({
-//   signin: user => ([
-//     ['fillIn', 'Name', user.name],
-//     ['click', {testId:"submit"}],
-//     ['see', "Welcome, " + user.name]
-//   ])})
+const common = ({
+  signin: user => ([
+    ['fillIn', 'Name', user.name],
+    ['click', {testId:"submit"}],
+    ['see', "Welcome, " + user.name]
+  ])})
 
 /*
  * Configure Marmot
@@ -49,42 +47,41 @@ window.MessageChannel = require('worker_threads').MessageChannel;
 
 
 
-// Marmot.root(() => <App />)
-// Marmot.router(() => history)
-// Marmot.renderer((child, opts) => withRouter(child, opts.route))
-// Marmot.renderer(child => withProvider(createMockStore({}))(child))
+Marmot.root(() => <App />)
+Marmot.router(() => history)
+Marmot.renderer((child, opts) => withRouter(child, opts.route))
+Marmot.renderer(child => withProvider(createMockStore({}))(child))
 
 // Fixture
-// const names = [
-//   "Billy Bob",
-//   "Bilbo Baggins"
-// ]
+const names = [
+  "Billy Bob",
+  "Bilbo Baggins"
+]
 
 // Run the scenario with cleanup
 describe("Account", () => {
 
-  // afterEach(Marmot.cleanup)
+  afterEach(Marmot.cleanup)
 
-  it("Sign In - w/ navigation", () => {})
-  // scenario("Sign In - w/ navigation", {route: "/session"})
-  //   .fillIn("Name", names[0])
-  //   .click({testId:"submit"})
-  //   .see("Welcome, " + names[0])
-  //   .run()
+  scenario("Sign In - w/ navigation", {route: "/session"})
+    .fillIn("Name", names[0])
+    .click({testId:"submit"})
+    .see("Welcome, " + names[0])
+    .run()
 
   // A second similar test to smoke out test-isolation issues (jsdom not clearing out), also, navigation after init
-  // scenario("Sign In - w/ testId")
-  //   .visit("/session")
-  //   .fillIn("Name", "Bilbo Baggins")
-  //   .click({testId:"submit"})
-  //   .see("Welcome, " + names[1])
-  //   .see({testId: "subtitle"})
-  //   .run()
-  //
-  // // An example of a scenario that uses predefined steps
-  // scenario("Sign In - w/ factory steps")
-  //   .steps(common.signin({name: "Dumbo Bumbo"}))
-  //   .see({testId: "subtitle"})
-  //   .run()
+  scenario("Sign In - w/ testId")
+    .visit("/session")
+    .fillIn("Name", "Bilbo Baggins")
+    .click({testId:"submit"})
+    .see("Welcome, " + names[1])
+    .see({testId: "subtitle"})
+    .run()
+
+  // An example of a scenario that uses predefined steps
+  scenario("Sign In - w/ factory steps")
+    .steps(common.signin({name: "Dumbo Bumbo"}))
+    .see({testId: "subtitle"})
+    .run()
 
 })
